2d29acdc4d1833d6e207b7c3f27b305f49d44fbd
szaneer@gmail.com <siraj@pal-nat186-10-4.itap.purdue.edu>
2017-09-05 15:14:09 -0400

    Project Initialized - Swift 4 - Xcode 9 Beta 6

---
+++ EventUp!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 9/5/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
