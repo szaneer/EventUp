a34b85a5bd1b6ce1bc9b0748048afc8102b3d8b6
Joey <jdafforn@purdue.edu>
2017-09-25 13:50:02 -0400

    Set up UI for displaying/creating events (add in buttons and set up page to receive information from Firebase)

---
+++ EventUp!/Events/EventCell.swift
@@ -0,0 +1,31 @@
+//
+//  EventCell.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 9/25/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    @IBOutlet weak var eventView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
