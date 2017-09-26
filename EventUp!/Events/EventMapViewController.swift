cf0e6199171f8a17ffd6ddc65310a8037f687d48
scarbone <scarbon@purdue.edu>
2017-09-26 15:05:22 -0400

    Create event location page

---
+++ EventUp!/Events/EventMapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  EventMapViewController.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 9/25/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class EventMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var eventMapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupLocation() {
+        eventMapView.delegate = self
+        eventMapView.showsUserLocation = true
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+        } else {
+            // Tell user to turn on location
+        }
+        
+        locationManager.delegate = self
+    }
+    
+    @IBAction func onLocation(_ sender: Any) {
+        if let userLocation = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            eventMapView.setRegion(region, animated: true)
+        }
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
