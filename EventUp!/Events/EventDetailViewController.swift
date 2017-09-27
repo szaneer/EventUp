4e29173029bd89ff01c43d0b214695629f042c34
scarbone <scarbon@purdue.edu>
2017-09-27 15:45:46 -0400

    Add event details, tags

---
+++ EventUp!/Events/EventDetailViewController.swift
@@ -0,0 +1,47 @@
+
+//
+//  EventDetailViewController.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 9/27/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class EventDetailViewController: UIViewController {
+    @IBOutlet weak var eventView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var attendeesLabel: UILabel!
+    @IBOutlet weak var eventMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
