b942cc7b2529dc0e0e60811fdd0a175649a605bf
Jackson Didat <jdidat@purdue.edu>
2017-09-30 20:05:26 -0400

    Create a page for all current and future events. Add sorting functionality

--- EventUp!/Events/EventsViewController.swift
+++ EventUp!/Events/EventsViewController.swift
@@ -50,7 +50,9 @@ class EventsViewController: UITableViewController {
         return cell
     }
     
-
+    @IBAction func onTimeSwitch(_ sender: Any) {
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
