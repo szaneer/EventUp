6f861dd73996ae0b475a345f8f2de6c52418b825
Siraj Zaneer <siraj@Sirajs-MacBook-Pro.local>
2017-11-07 13:08:59 -0800

    Design a welcome screen that greets the user after they log in.

---
+++ EventUp!/Main/WelcomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  WelcomeViewController.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 11/7/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 2, animations: {
+            
+        }) { (true) in
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
