51a14b9b31a9031101e3e558be71547525b9be09
Deadlocked007 <szaneer@gmail.com>
2017-11-14 19:26:06 -0600

    Created sidebar button and implmented sidebar

---
+++ EventUp!/Events/EventContainerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EventContainerViewController.swift
+//  EventUp!
+//
+//  Created by Siraj Zaneer on 11/20/17.
+//  Copyright © 2017 Siraj Zaneer. All rights reserved.
+//
+
+import UIKit
+
+class EventContainerViewController: SOContainerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
